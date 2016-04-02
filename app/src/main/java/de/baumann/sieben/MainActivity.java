a33f5bfdf2ef6103a372797f2928255c0d3e1652
scoute-dich <baumann.juergen@posteo.de>
2016-04-02 18:53:11 +0200

    cleanup github

--- app/src/main/java/de/baumann/sieben/MainActivity.java
+++ app/src/main/java/de/baumann/sieben/MainActivity.java
@@ -278,6 +278,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //Test
+
     @Override
     public void onBackPressed() {
         isCanceled = true;
